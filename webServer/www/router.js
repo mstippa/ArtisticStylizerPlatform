c63c868dcd439f5a40dbd46276a3a5e0c15fe264
mstippa <ebinizerlop@gmail.com>
2017-10-03 16:57:04 -0400

    updating the webServer repository

--- webServer/www/router.js
+++ webServer/www/router.js
@@ -2,7 +2,7 @@
 // these are the file system (fs) and url modules that come as part of
 // node.js
 var url = require('url');
-var fs = require('fs'); 
+var fs = require('fs');
 
 //this allows our server file (server.js) to use this get(request,response)
 //function, passing the request and response objects through
@@ -10,7 +10,8 @@ exports.get = function(request, response){
 	//getting the path of the URL request
 	request.requrl = url.parse(request.url, true);
 	var path = request.requrl.pathname;
-	
+	path = path.toLowerCase();
+
 	//testing to see whether the request is for a CSS file and loading it in if
 	//so.
 	if(/.(css)$/.test(path)){
@@ -22,12 +23,33 @@ exports.get = function(request, response){
 				response.write(data, 'utf8');
 				response.end();
 		});
+	}else if(/.(jpg)$/.test(path) || /.(png)$/.test(path)){
+                // get the img
+		var img = fs.readFileSync(path.substring(1));
+
+		response.writeHead(200, {
+                                'Content-Type': 'image/gif'
+                });
+		response.end(img, 'binary');
+
+        }else if(/.(js)$/.test(path) ){
+		response.writeHead(200, {
+			'Content-Type': 'text/javascript'
+		});
+		fs.readFile(__dirname +path, 'utf8', function(err, data){
+				if(err) throw err;
+				response.write(data,'utf8');
+				response.end();
+		});
 	}else{
 		//sending the request to the correct controller if not CSS file
 		if(path === '/' || path === '/home'){
 			require('./controllers/home').get(request, response);
-		}else{
+		} else if (path === '/about') {
+			require('./controllers/about-controller').get(request, response);
+		} else if (path === '/login') {
+				require('./controllers/login-controller').get(request, response);
+		}else
 			require('./controllers/404').get(request, response);
 		}
-	}
-};
+}
--- webServer/www/views/template-main.js
+++ webServer/www/views/template-main.js
@@ -1,8 +1,8 @@
 /*
-The first thing we are doing here is joining an array of strings, placing 
-a new line ‘n’ between them. This creates one long string of HTML that will 
-render on separate lines leaving the source code more readable. This string is 
-the basis of our template, and contains a number of placeholders for the 
+The first thing we are doing here is joining an array of strings, placing
+a new line ï¿½nï¿½ between them. This creates one long string of HTML that will
+render on separate lines leaving the source code more readable. This string is
+the basis of our template, and contains a number of placeholders for the
 content items {title}, {pagetitle} and {content}.
 */
 exports.build = function(title, pagetitle, content){
