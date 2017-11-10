2ef3d5d7f264950fbc8baa0a29654ab6bf26be1d
Michael <michael.guarino1@marist.edu>
2017-11-10 14:04:05 -0500

    Update upload-video-controller.js
    update video controller still must update path

--- webServer/www/controllers/upload-video-controller.js
+++ webServer/www/controllers/upload-video-controller.js
@@ -11,28 +11,18 @@
 
 //things we need -- the test data and the homepage html template
 var User = require('../model/user');
-var html = require('../views/view-home');
+var formidable = require('formidable');
+var fs = require('fs');
 
-
-
-exports.get = function(request, response, info) {
-
-	//put in the headers that we were successful
-	// response.writeHead(200, {
-	// 		'Content-Type':'text/html'
-	// });
-
-	// response.render("../views/login.ejs");
-	return response.render("../views/login.ejs", {message: info});
-};
-
-exports.post = function(request, response, info){
-	console.log(info);
-	return response.render('../views/login.ejs', {message:info});
+exports.post = function(req, res){
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files){
+		var oldpath = files.filetoupload.path;
+		var newpath = '/home/mike/' + files.filetoupload.name;
+		fs.rename(oldpath, newpath, function(err){
+			if (err) throw err;
+			res.write('files uploaded and moved!');
+			res.end();
+		});
+	});
 }
-
-//this is for updating the page when they hit the login button
-// exports.post = function(request, response){
-// 	request.render()
-// 	response.end();
-// };
