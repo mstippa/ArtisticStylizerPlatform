c15bf691880ef86a6794b377ee40b36558a51bb2
Michael <michael.guarino1@marist.edu>
2017-11-14 10:14:38 -0500

    Update upload-image-controller.js
    multi image uploader and using python shell to call style transfer algorithm

--- webServer/www/controllers/upload-image-controller.js
+++ webServer/www/controllers/upload-image-controller.js
@@ -11,9 +11,11 @@
 
 //things we need -- the test data and the homepage html template
 var User = require('../model/user');
-var formidable = require('formidable');
-var fs = require('fs');
+var multer = require('multer');
+var upload = multer({dest: 'tmp/'})
+var PythonShell = require('python-shell');
 
+/*
 exports.post = function(req, res){
 	var form = new formidable.IncomingForm();
 	form.parse(req, function(err, fields, files){
@@ -26,3 +28,42 @@ exports.post = function(req, res){
 		});
 	});
 }
+*/
+
+var storage = multer.diskStorage({
+	destination: function(req, res, callback){
+		callback(null, 'tmp/');
+	},
+
+	filename: function(req, file, callback){
+		console.log(file);
+		callback(null, file.originalname)
+	}
+});
+
+var upload = multer({storage: storage}).array('photo', 2);
+
+exports.post = function(req, res){
+	upload(req, res, function(err){
+		if(err){
+			console.log('error occured');
+			return;
+		}
+		// req.files is an ojbect where fieldname is the key and value is the array of files
+		console.log(req.files);
+		console.log('photo uploaded');
+		var options = {
+			pythonPath: '/usr/bin/python3',
+		        scriptPath: '/home/mike/ArtisticStylizerPlatform/gpuServer/AS/src',
+		        args: [req.files[0].path, req.files[1].path, '/home/mike/results', 256, 512]
+		};
+		try{
+			PythonShell.run('inference_master.py', options, function(err){
+				if (err) throw err;
+			});
+		}
+		catch(err){
+			console.log(err);
+		}
+	});
+};
