8862eb29dbd9d03da2a8d0a96a867858aef6dc9c
tony <soricellia@gmail.com>
2017-11-29 19:45:40 -0500

    added default styles and inserted them into the database. added a insert sql file for automation

---
+++ webServer/www/model/asp_insert_default_styles.sql
@@ -0,0 +1,42 @@
+/*************************************************************
+	ASP DATABASE INSERT DEFAULT STYLES
+	
+	Purpose:
+		-Used to source default styles into the database
+
+
+	Author:			Anthony Soricelli
+	Last edit:		11/29/2017
+
+*************************************************************/
+
+-- use asp database so we can do things with it
+use asp_database;
+
+/**************************************
+*		INSERTING DEAULT STYLES		  *
+**************************************/
+
+INSERT INTO styles(style_path)
+	VALUES ("~/ArtisticStylizerPlatform/webServer/www/public/default_styles/alien_rainbow.jpg");
+
+INSERT INTO styles(style_path)
+	VALUES ("~/ArtisticStylizerPlatform/webServer/www/public/default_styles/brickwall.jpg");
+
+INSERT INTO styles(style_path)
+	VALUES ("~/ArtisticStylizerPlatform/webServer/www/public/default_styles/deja_vu.jpg");
+
+INSERT INTO styles(style_path)
+	VALUES ("~/ArtisticStylizerPlatform/webServer/www/public/default_styles/divisionism.jpg");
+
+INSERT INTO styles(style_path)
+	VALUES ("~/ArtisticStylizerPlatform/webServer/www/public/default_styles/instagael.jpg");
+
+INSERT INTO styles(style_path)
+	VALUES ("~/ArtisticStylizerPlatform/webServer/www/public/default_styles/oil_on_canvas.jpg");
+
+INSERT INTO styles(style_path)
+	VALUES ("~/ArtisticStylizerPlatform/webServer/www/public/default_styles/van_gogh.jpg");
+
+INSERT INTO styles(style_path)
+	VALUES ("~/ArtisticStylizerPlatform/webServer/www/public/default_styles/water_ripple.jpg");
