1400234893cb1acfee0f3589a2997dcef8ac62df
Michael <michael.guarino1@marist.edu>
2017-12-02 18:23:00 -0500

    operational processManager

--- webServer/www/scripts/processManager.py
+++ webServer/www/scripts/processManager.py
@@ -4,8 +4,7 @@ import subprocess
 import re
 import pandas as pd
 import six
-
-
+import datetime
 
 logger = logging.getLogger("processManager")
 logger.setLevel(logging.INFO)
@@ -18,16 +17,10 @@ fileHandler.setFormatter(formatter)
 logger.addHandler(fileHandler)
 
 cw_dir = os.getcwd()
-gpuServer_dir = os.path.join("/".join(cw_dir.split("/")[:-2]), "gpuServer/AS/src")
+style_transfer_dir = os.path.join(cw_dir, "style_transfer/src")
 
-if six.PY2:
-  logger.info("python2")
-else:
-  logger.info("python3")
-
-def get_tf_status(dir=gpuServer_dir, filename="inference_master.log"):
+def get_tf_status(filename="inference_master.log"):
   logger.info("get_tf_status")
-  #last_line = subprocess.check_output(['tail', '-1', os.path.join(dir, filename)])
   last_line = subprocess.check_output(['tail', '-1', filename])
   pattern = re.compile(b"inference_master:end style transfer")
   if re.search(pattern, last_line):
@@ -39,12 +32,23 @@ def get_tf_status(dir=gpuServer_dir, filename="inference_master.log"):
   return tf_free
 # end
 
-def load_json(file):
+def load_json(file, orient=True):
   logger.info("load_json")
   logger.info(file)
-  persisted_processes = pd.read_json(file, orient="records")
+  if orient:
+    persisted_processes = pd.read_json(file, orient="records")
+  else:
+    persisted_processes = pd.read_json(file)
   logger.info(persisted_processes)
   return persisted_processes
+# end
+
+def remove_min_elm(file, df, min_rank):
+  df = df.drop(df[df["queue_rank"] == min_rank].index)
+  if df.empty:
+    os.remove(file)
+  else:
+    df.to_json(file, orient="records")
 # end
 
 def get_min_queue_rank_elm(df):
@@ -55,12 +59,13 @@ def get_min_queue_rank_elm(df):
   logger.info(min_elm_df)
   min_elm_dict = min_elm_df.to_dict(orient="records")
   logger.info(min_elm_dict)
-  return min_elm_dict
+  return min_rank, min_elm_dict
 # end
 
-def start_tf_process(data_dict, dir=gpuServer_dir, filename="inference_master.py"):
+def start_tf_process(data_dict, dir=style_transfer_dir, filename="inference_master.py"):
   logger.info("start_tf_process")
   logger.info(os.path.join(dir, filename))
+  logger.info(data_dict)
   logger.info(data_dict[0]["content_img_path"])
   logger.info(type(data_dict[0]["final_size"]))
   logger.info(type(data_dict[0]["transient_size"]))
@@ -68,21 +73,45 @@ def start_tf_process(data_dict, dir=gpuServer_dir, filename="inference_master.py
                     os.path.join(dir, filename),
                     data_dict[0]["content_img_path"], 
                     data_dict[0]["style_img_path"],
-                    data_dict[0]["result_img_path"],
+                    str(data_dict[0]["result_img_path"]),
                     str(data_dict[0]["final_size"]),
                     str(data_dict[0]["transient_size"])), shell=True, stdout=subprocess.PIPE).wait()
   logger.info("start_tf_process end")
 # end
 
-logger.info("############## next ############")
+def run(queue_file):
+  queue_df = load_json(file=queue_file)
+  min_rank, min_elm_dict = get_min_queue_rank_elm(df=queue_df)
+  start_time = datetime.datetime.now() # time before style transfer 
+  start_tf_process(data_dict=min_elm_dict)
+  end_time = datetime.datetime.now() # time after style transfer
+  remove_min_elm(file=queue_file, df=queue_df, min_rank=min_rank)
+  # print out for nodejs event listener
+  print("start_time: {},\
+    end_time: {},\
+    content_size: {},\
+    profile_id: {},\
+    style_id: {},\
+    premium_style_id: {},\
+    content_path: {}".format(start_time,
+                             end_time,
+                             str(min_elm_dict[0]["final_size"]),
+                             "profile_id",
+                             "style_id",
+                             "premium_style_id",
+                             min_elm_dict[0]["content_img_path"]))
+# end
+
 while True:
-  tf_free = get_tf_status()
-  logger.info("after checking tf status")
-  if tf_free:
-    logger.info("tf is free")
-    queue_df = load_json(file="process_queue.json")
-    min_elm_dict = get_min_queue_rank_elm(df=queue_df)
-    start_tf_process(data_dict=min_elm_dict)
-  exit(0)
-  #else:
-    #wait
+  # check if queue exits 
+  queue_file = "process_queue.json"
+  if queue_file in os.listdir(os.getcwd()):
+    if "inference_master.log" in os.listdir(os.getcwd()):
+      tf_free = get_tf_status()
+      if tf_free:
+        logger.info("tf is free")
+        run(queue_file=queue_file)
+    else:
+      run(queue_file=queue_file)
+  else: 
+    continue
