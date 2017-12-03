b6159c1cfbd6b584646bdbc7d667ed9478ba2da1
Michael <michael.guarino1@marist.edu>
2017-12-03 15:40:38 -0500

    updating processManager
    updating processManager

--- webServer/www/scripts/processManager.py
+++ webServer/www/scripts/processManager.py
@@ -6,7 +6,6 @@ import pandas as pd
 import six
 import datetime
 
-
 logger = logging.getLogger("processManager")
 logger.setLevel(logging.INFO)
 
@@ -18,12 +17,10 @@ fileHandler.setFormatter(formatter)
 logger.addHandler(fileHandler)
 
 cw_dir = os.getcwd()
-
 style_transfer_dir = os.path.join(cw_dir, "style_transfer/src")
 
 def get_tf_status(filename="inference_master.log"):
   logger.info("get_tf_status")
-
   last_line = subprocess.check_output(['tail', '-1', filename])
   pattern = re.compile(b"inference_master:end style transfer")
   if re.search(pattern, last_line):
@@ -43,7 +40,6 @@ def load_json(file):
   return persisted_processes
 # end
 
-
 def remove_min_elm(file, df, min_rank):
   df = df.drop(df[df["queue_rank"] == min_rank].index)
   if df.empty:
@@ -51,7 +47,6 @@ def remove_min_elm(file, df, min_rank):
   else:
     df.to_json(file, orient="records")
 # end
-
 
 def get_min_queue_rank_elm(df):
   logger.info("min_queue_rank")
@@ -61,7 +56,6 @@ def get_min_queue_rank_elm(df):
   logger.info(min_elm_df)
   min_elm_dict = min_elm_df.to_dict(orient="records")
   logger.info(min_elm_dict)
-
   return min_rank, min_elm_dict
 # end
 
@@ -69,7 +63,6 @@ def start_tf_process(data_dict, dir=style_transfer_dir, filename="inference_mast
   logger.info("start_tf_process")
   logger.info(os.path.join(dir, filename))
   logger.info(data_dict)
-
   logger.info(data_dict[0]["content_img_path"])
   logger.info(type(data_dict[0]["final_size"]))
   logger.info(type(data_dict[0]["transient_size"]))
@@ -77,14 +70,11 @@ def start_tf_process(data_dict, dir=style_transfer_dir, filename="inference_mast
                     os.path.join(dir, filename),
                     data_dict[0]["content_img_path"], 
                     data_dict[0]["style_img_path"],
-
                     str(data_dict[0]["result_img_path"]),
-
                     str(data_dict[0]["final_size"]),
                     str(data_dict[0]["transient_size"])), shell=True, stdout=subprocess.PIPE).wait()
   logger.info("start_tf_process end")
 # end
-
 
 def run(queue_file):
   queue_df = load_json(file=queue_file)
@@ -121,4 +111,3 @@ while True:
           run(queue_file=queue_file)
       else:
         run(queue_file=queue_file)
-
